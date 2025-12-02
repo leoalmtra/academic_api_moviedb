2d413168c2e96780e85a06ff1c5ec7d31dae5221
Leonardo Dutra <contatodutraleo@gmail.com>
2025-12-02 00:10:21 -0300

    Update main.py
    Adjusted the fallback on main

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ from getapi import consultar_filme
 if __name__ == "__main__":
     nome = input("Digite o nome do filme: ")
     resultado = consultar_filme(nome)
-    print(f"Titulo: {resultado['titulo']}")
-    print(f"Ano: {resultado['ano']}")
-    print(f"Sinopse: {resultado['sinopse']}")
-
+    
+    if "erro" in resultado:
+        print(f"ERRO: {resultado['erro']}")
+    else:
+        print(f"Titulo: {resultado['titulo']}")
+        print(f"Ano: {resultado['ano']}")
+        print(f"Sinopse: {resultado['sinopse']}")
